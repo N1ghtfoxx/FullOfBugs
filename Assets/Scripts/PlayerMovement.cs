63071649279b1b71d70df6da2c29c7738205c2f3
Jo <joyce.cosmicgoblin@gmail.com>
2026-06-01 14:10:03 +0200

    [ADD] Animation Player
    Walking (Animation) + Idle (no Animation)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,15 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public Animator anim;
+
+    [SerializeField] private bool walking;
     
     Vector2 movementInput;
     [SerializeField] private float moveSpeed = 5f;
@@ -19,10 +24,31 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = movementInput * moveSpeed;
+        Animate();
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
         movementInput = ctx.ReadValue<Vector2>();
+        //Animate();
+    }
+
+    public void Animate()
+    {
+        if (movementInput.magnitude > 0.1f || movementInput.magnitude < -0.1f)
+        {
+            walking = true;
+        }
+        else
+            walking = false;
+
+        if (walking)
+        {
+            anim.SetFloat("X", movementInput.x);
+            anim.SetFloat("Y", movementInput.y);
+        }
+
+        anim.SetBool("Walking", walking);
+
     }
 }
