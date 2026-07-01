e00a780e3a6a2e5fbbb08525d08c5c42406b4a07
1stTry2punkt0 <philonhauk@gmail.com>
2026-07-01 09:52:32 +0200

    ADD Confetti to victory screen

--- Assets/Scripts/Ending/Exit.cs
+++ Assets/Scripts/Ending/Exit.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Exit : HermbertEntered
 {
     [SerializeField] GameObject _victoryScreen;
+    [SerializeField] GameObject _confetti;
 
     protected override void Interaction()
     {
         base.Interaction();
         Debug.Log("Victory");
         _victoryScreen.SetActive(true);
+        _confetti.SetActive(true);
     }
 }
